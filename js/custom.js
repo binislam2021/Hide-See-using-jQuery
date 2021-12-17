a85f616d3d667cf1a751d1b2f2457caa055a6194
binislam2021 <contactwithmdismail@gmail.com>
2021-12-18 00:10:00 +0600

    Slide Up/Down Added

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,4 @@
-// Hide & See using jQuery
-
+// Hide & Show using jQuery
 $("#btn1").click(function(){
     $("#para").hide();
 });
@@ -12,4 +11,20 @@ $("#btn2").click(function(){
 
 $("#btn3").click(function(){
     $("#para").toggle();
+});
+
+
+// SlidUp & SlideDown
+$("#btn4").click(function(){
+    $("#para").slideUp();
+});
+
+
+$("#btn5").click(function(){
+    $("#para").slideDown();
+});
+
+
+$("#btn6").click(function(){
+    $("#para").slideToggle();
 });
