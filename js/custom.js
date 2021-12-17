b883e356fee7670f3a643d6b3ac5f71b80f0ec8f
binislam2021 <contactwithmdismail@gmail.com>
2021-12-17 23:46:49 +0600

    jQuery introduction Class/practice

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+// Hide & See using jQuery
+
+$("#btn1").click(function(){
+    $("#para").hide();
+});
+
+
+$("#btn2").click(function(){
+    $("#para").show();
+});
+
+
+$("#btn3").click(function(){
+    $("#para").toggle();
+});
